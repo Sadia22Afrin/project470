8e5eb0ccb8efe93ac718e772707a2e386151181a
Sadia22Afrin <121007060+Sadia22Afrin@users.noreply.github.com>
2022-12-19 21:55:22 +0600

    Add files via upload

---
+++ View/UI.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+
+<style> 
+	
+	
+
+    Body {  
+      font-family: Calibri, Helvetica, sans-serif;  
+	  background:url(https://images.pexels.com/photos/104826/aircraft-holiday-sun-tourism-104826.jpeg?cs=srgb&dl=pexels-pixabay-104826.jpg&fm=jpg&_gl=1*9vah89*_ga*MTQxNTk5NDQxMi4xNjcxNDMyMjUx*_ga_8JE65Q40S6*MTY3MTQ1OTgwOS4yLjEuMTY3MTQ2MDQ3OS4wLjAuMA..) no-repeat center fixed;
+	  background-size: cover;
+	  
+    button {   
+           background-color: #03052e;   
+           width: 25%;  
+            color: green;   
+            padding: 12px;
+            margin: 0px 80px;   
+            border: 4px black;   
+            cursor: pointer; 
+            
+             }   
+      form {   
+            position: fixed;  
+            top: 320px;
+            left: 725px;
+            
+            width: 25%;
+            height: 28%;
+        }   
+     input[type=password] {   
+            width: 35%;   
+            margin: 15px 15px;  
+            padding: 12px 20px;   
+            display: inline-block;
+            border: 14px black;   
+            box-sizing: border-box;
+            opacity: 0.7   
+        }  
+     input[type=text] {   
+            width: 35%;   
+            margin: 15px 10px;  
+            padding: 12px 20px;   
+            display: inline-block;
+            border: 14px black;   
+            box-sizing: border-box;
+            opacity: 0.7   
+        }  
+     button:hover {   
+            opacity: 0.7;   
+        }   
+
+              .container { 
+            padding: 10px;
+            background-color: #0000000.5;  
+        }
+	}		
+		  </style>
+</html>
